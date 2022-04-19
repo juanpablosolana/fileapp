02a8c65c977211dbcaba4c95ece2398d74787716
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-04-18 21:39:20 -0500

    status service ready

---
+++ pages/api/status.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  const status = () => {
+    fetch('https://api.gofile.io/getServer')
+      .then(response => response.json())
+      .then(data => {
+        if(data.status === 'ok') {
+        res.status(200).json({ data })
+        }
+        res.status(500).json({ error })
+      })
+  }
+  status()
+}
